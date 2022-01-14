b61687f7c0e3864e63305288e404778b09c55740
Dominic Roudabush <91146334+roudabushdominic@users.noreply.github.com>
2022-01-14 12:39:14 -0500

    Update Figure8.js

--- Figure8.js
+++ Figure8.js
@@ -4,20 +4,20 @@ Date:1/13/2022
 Program: Figure 8
 */
 
-async function upperLoop(){
-		setSpeed(80);
-		await spin(355,4.5);
-		stop roll();
+async function upperLoop(){					
+		setSpeed(86);				// sets the speed for the Sphero to move 
+		await spin(349,5);			// makes sphero change to direction to make the upperloop 
+		stop roll();				//stops roll 
 }
 
 async function lowerLoop(){
-		setSpeed(80);
-		await spin(-355,4.5);
-		stop roll();
+		setSpeed(86);				// sets the speed of the Sphero to move 
+		await spin(-349,5);			// makes sphero change direction to make the lowerloop 
+		stop roll();				// stops roll 
 }
 
 async function startProgram(){
-		await upperLoop();
-		await lowerLoop();
+		await upperLoop();			// call upperLoop Function 
+		await lowerLoop();			// call lowerLoop Function 
 }
 		
