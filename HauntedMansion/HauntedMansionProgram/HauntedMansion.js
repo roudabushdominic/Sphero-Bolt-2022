3dff6c60f9723545c4c0c9b4f288413992b599de
Dominic <roudabushcs@gmail.com>
2022-01-26 09:36:26 -0800

    Initial Haunted Mansion Commit

---
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -0,0 +1,7 @@
+/*
+Programmer: Dominic Roudabush
+Team Member: Brendan 
+Date: 1.26.2022
+Program: We will be making a prrogram to go through a haunted Mansion maze filled wth hauted sounds, changing of colors, and other obstacles along the way using a sphero bolt abd Java script
+*/
+
